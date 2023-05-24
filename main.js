b07520de49e7ef6006ff509fb75a65354b6cb468
Shubham Sharma <s_sharma2@me.iitr.ac.in>
2023-05-24 17:43:35 +0530

    2commit

--- main.js
+++ main.js
@@ -2,12 +2,3 @@ import './style.css'
 import Experience from './Experience/Experience'
 
 const experience = new Experience(document.querySelector('.experience-canvas'))
-let count = 0;
-if(window.pageYOffset==0){
-    count=1;
-
-
-}
-if(count===1)
-{
-    document.getElementsByClassName('intro-text').style.opacity = 1;}
